eeea1818310d5138c4dfb708826c58080871a6d4
CHINAN CHENG <CHINAN CHENG>
2017-05-12 23:47:50 +0800

    1.add chatroom and hidden chatroom('@') 2.add members display 3.delete members uncomplete 4. add delete group

---
+++ src/model/groups.js
@@ -0,0 +1,142 @@
+const fs = require('fs');
+const uuid = require('uuid/v4');
+const moment = require('moment');
+
+function list(searchText = '') {
+    return new Promise((resolve, reject) => {
+        if (!fs.existsSync('data-groups.json')) {
+            fs.writeFileSync('data-groups.json', '');
+        }
+
+        fs.readFile('data-groups.json', 'utf8', (err, data) => {
+            if (err) reject(err);
+
+            let groups = data ? JSON.parse(data) : [];
+            if (groups.length > 0 && searchText) {
+                groups = groups.filter(p => {
+                    return p.name.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+                });
+            }
+            resolve(groups);
+        });
+    });
+}
+
+function create(name) {
+    return new Promise((resolve, reject) => {
+        const newGroup = {
+            id: uuid(),
+            name: name,
+            ts: moment().unix(),
+            usernames: []
+        };
+
+        list().then(groups => {
+            groups = [
+                newGroup,
+                ...groups
+            ];
+            fs.writeFile('data-groups.json', JSON.stringify(groups), err => {
+                if (err) reject(err);
+
+                resolve(newGroup);
+            });
+        });
+    });
+}
+
+function get(id) {
+    return new Promise((resolve, reject) => {
+        if (!fs.existsSync('data-groups.json')) {
+            fs.writeFileSync('data-groups.json', '');
+        }
+
+        fs.readFile('data-groups.json', 'utf8', (err, data) => {
+            if (err) reject(err);
+
+            let groups = data ? JSON.parse(data) : [];
+            if (groups.length > 0) {
+                groups = groups.filter(p => {
+                    if(p.id === id)
+                      return p
+                });
+            }
+
+            resolve(groups);
+        });
+    });
+}
+
+function addMembers(id, username) {
+    return new Promise((resolve, reject) => {
+        const newUsername = {username: username};
+        list().then(groups => {
+            groups = groups.map(p => {
+                if (p.id === id) {
+                    p.usernames = [
+                      newUsername,
+                      ...p.usernames];
+                }
+
+                return p
+            });
+            fs.writeFile('data-groups.json', JSON.stringify(groups), err => {
+                if (err) reject(err);
+
+                resolve(groups);
+            });
+        });
+    });
+}
+
+function deleteMembers(id, username) {
+    return new Promise((resolve, reject) => {
+
+        list().then(groups => {
+            var obj = {username: username};
+            groups = groups.map(p => {
+                if (p.id === id) {
+                    p.usernames.filter(function (item) {
+                        if(item.username!==this.username){
+                          return true;
+                        }
+                    }, obj);
+
+                }
+
+                return p
+            });
+            fs.writeFile('data-groups.json', JSON.stringify(groups), err => {
+                if (err) reject(err);
+
+                resolve(groups);
+            });
+        });
+    });
+}
+
+function delete_(id) {
+  return new Promise((resolve, reject) => {
+      list().then(groups => {
+          var obj = {id: id};
+          groups = groups.filter(function (item) {
+              if(item.id!==this.id)
+                return true;
+          }, obj);
+          fs.writeFile('data-groups.json', JSON.stringify(groups), err => {
+              if (err) reject(err);
+
+              resolve(groups);
+          });
+      });
+  });
+}
+
+module.exports = {
+    list,
+    create,
+    addMembers,
+    deleteMembers,
+    get,
+    delete_
+};
--- src/routers/posts.js
+++ src/routers/posts.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const postModel = require('../model/posts.js');
+const groupModel = require('../model/groups.js');
+const chatroomModel = require('../model/chatroom.js');
 const voteModel = require('../model/votes.js');
 const todoModel = require('../model/todos.js');
 
@@ -43,38 +45,130 @@ router.post('/posts/:id/:mood(clear|clouds|drizzle|rain|thunder|snow|windy)Votes
 });
 /****************************************************************************************/
 
-// list todo 
-router.get('/todos', function(req, res, next) {
-    todoModel.listTodos(req.query.unaccomplishedOnly, req.query.searchText).then(posts => {
-        res.json(posts);
+// List
+router.get('/groups', function(req, res, next) {
+    groupModel.list(req.query.searchText).then(groups => {
+        res.json(groups);
     }).catch(next);
 });
 
-// create todo 
-router.post('/todos', function(req, res, next) {
-    const {mood, text} = req.body;
-    if (!mood || !text) {
-        const err = new Error('Mood and text are required');
+// Create
+router.post('/groups', function(req, res, next) {
+    const {name} = req.body;
+    if (!name) {
+        const err = new Error('Name are required');
         err.status = 400;
         throw err;
     }
-    todoModel.createTodo(mood, text).then(post => {
-        res.json(post);
+    groupModel.create(name).then(group => {
+        res.json(group);
     }).catch(next);
 });
 
-// accomplihs todo 
-router.post('/todos/:id', function(req, res, next) {
+// Get
+router.get('/groups/:id', function(req, res, next) {
     const {id} = req.params;
     if (!id) {
         const err = new Error('Post ID is required');
         err.status = 400;
         throw err;
     }
-    todoModel.accomplishTodo(id).then(post => {
-        res.json(post);
+    groupModel.get(id).then(groups => {
+        res.json(groups);
     }).catch(next);
 });
+
+// addMembers
+router.post('/groups/members/add', function(req, res, next) {
+    const {id, username} = req.body;
+    if (!id || !username) {
+        const err = new Error('Name are required');
+        err.status = 400;
+        throw err;
+    }
+    groupModel.addMembers(id, username).then(groups => {
+        res.json(groups);
+    }).catch(next);
+});
+
+// deleteMembers
+router.post('/groups/members/delete', function(req, res, next) {
+    const {id, username} = req.body;
+    if (!id || !username) {
+        const err = new Error('Name are required');
+        err.status = 400;
+        throw err;
+    }
+    groupModel.deleteMembers(id, username).then(groups => {
+        res.json(groups);
+    }).catch(next);
+});
+// Delete
+router.post('/groups/delete', function(req, res, next) {
+    const {id} = req.body;
+    if (!id) {
+        const err = new Error('GROUPID is required');
+        err.status = 400;
+        throw err;
+    }
+    groupModel.delete_(id).then(group => {
+        res.json(group);
+    }).catch(next);
+});
+
+/*****************************chats******************************************/
+// List
+router.post('/chats/list', function(req, res, next) {
+    const {id, searchtext} = req.body;
+    if (!id) {
+        const err = new Error('Name are required');
+        err.status = 400;
+        throw err;
+    }
+    chatroomModel.list(id, searchtext).then(chats => {
+        res.json(chats);
+    }).catch(next);
+});
+
+// Create
+router.post('/chats/create', function(req, res, next) {
+    const {id, username, text} = req.body;
+    if (!id || !username || !text) {
+        const err = new Error('Name are required');
+        err.status = 400;
+        throw err;
+    }
+    chatroomModel.create(id, username, text).then(chat => {
+        res.json(chat);
+    }).catch(next);
+});
+/****************************hid***********************************/
+// List
+router.post('/chats/list/hid', function(req, res, next) {
+    const {id, searchtext} = req.body;
+    if (!id) {
+        const err = new Error('Name are required');
+        err.status = 400;
+        throw err;
+    }
+    chatroomModel.list_hid(id, searchtext).then(chats => {
+        res.json(chats);
+    }).catch(next);
+});
+
+// Create
+router.post('/chats/create/hid', function(req, res, next) {
+    const {id, username, text} = req.body;
+    if (!id || !username || !text) {
+        const err = new Error('Name are required');
+        err.status = 400;
+        throw err;
+    }
+    chatroomModel.create_hid(id, username, text).then(chat => {
+        res.json(chat);
+    }).catch(next);
+});
+
 
 
 module.exports = router;
